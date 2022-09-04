e4c1b45871de32b984d8fbf782757c066017cf07
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-09-05 00:11:29 +0900

    test: users resolver

--- fx-di-gql-ent/resolver/user.resolvers_test.go
+++ fx-di-gql-ent/resolver/user.resolvers_test.go
@@ -19,16 +19,12 @@ func TestUser(t *testing.T) {
 			id int
 		}
 
-		type want struct {
-			user *ent.User
-			err  error
-		}
-
 		tests := []struct {
 			name string
 			mock func() *mock_service.MockUserService
 			params
-			want want
+			want *ent.User
+			err  error
 		}{
 			{
 				name: "Normal time, can get user",
@@ -41,11 +37,12 @@ func TestUser(t *testing.T) {
 					return m
 				},
 				params: params{id: 1},
-				want: want{user: &ent.User{
+				want: &ent.User{
 					ID:    1,
 					Name:  "test user",
 					Email: "test@example.com",
-				}, err: nil},
+				},
+				err: nil,
 			},
 			{
 				name: "Error time, can't get user",
@@ -54,7 +51,8 @@ func TestUser(t *testing.T) {
 					return m
 				},
 				params: params{id: 1},
-				want:   want{user: nil, err: errors.New("some error")},
+				want:   nil,
+				err:    errors.New("some error"),
 			},
 		}
 
@@ -62,8 +60,55 @@ func TestUser(t *testing.T) {
 			t.Run(tt.name, func(t *testing.T) {
 				qr := &queryResolver{Resolver: &Resolver{userService: tt.mock()}}
 				got, err := qr.User(ctx, tt.params.id)
-				assert.Equal(t, tt.want.user, got)
-				assert.Equal(t, tt.want.err, err)
+				assert.Equal(t, tt.want, got)
+				assert.Equal(t, tt.err, err)
+			})
+		}
+	})
+}
+
+func TestUsers(t *testing.T) {
+	withDIContainer(t, func(m *mock_service.MockUserService) {
+		ctx := context.Background()
+
+		tests := []struct {
+			name string
+			mock func() *mock_service.MockUserService
+			want []*ent.User
+			err  error
+		}{
+			{
+				name: "Normal time, can get all users",
+				mock: func() *mock_service.MockUserService {
+					m.EXPECT().FindAll(gomock.Eq(ctx)).Return([]*ent.User{
+						{ID: 1, Name: "test1", Email: "test1@example.com"},
+						{ID: 2, Name: "test2", Email: "test2@example.com"},
+					}, nil)
+					return m
+				},
+				want: []*ent.User{
+					{ID: 1, Name: "test1", Email: "test1@example.com"},
+					{ID: 2, Name: "test2", Email: "test2@example.com"},
+				},
+				err: nil,
+			},
+			{
+				name: "Error time, can't get user",
+				mock: func() *mock_service.MockUserService {
+					m.EXPECT().FindAll(gomock.Eq(ctx)).Return(nil, errors.New("some error"))
+					return m
+				},
+				want: nil,
+				err:  errors.New("some error"),
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				qr := &queryResolver{Resolver: &Resolver{userService: tt.mock()}}
+				got, err := qr.Users(ctx)
+				assert.Equal(t, tt.want, got)
+				assert.Equal(t, tt.err, err)
 			})
 		}
 	})
