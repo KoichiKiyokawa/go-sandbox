97b8f145110edf8b1ba4e268635389306ad50060
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-09-05 00:00:02 +0900

    test: create `withDIContainer` helper

--- fx-di-gql-ent/resolver/user.resolvers_test.go
+++ fx-di-gql-ent/resolver/user.resolvers_test.go
@@ -12,61 +12,59 @@ import (
 )
 
 func TestUser(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	withDIContainer(t, func(m *mock_service.MockUserService) {
+		ctx := context.Background()
 
-	m := mock_service.NewMockUserService(ctrl)
-	ctx := context.Background()
+		type params struct {
+			id int
+		}
 
-	type params struct {
-		id int
-	}
+		type want struct {
+			user *ent.User
+			err  error
+		}
 
-	type want struct {
-		user *ent.User
-		err  error
-	}
-
-	tests := []struct {
-		name string
-		mock func() *mock_service.MockUserService
-		params
-		want want
-	}{
-		{
-			name: "Normal time, can get user",
-			mock: func() *mock_service.MockUserService {
-				m.EXPECT().FindOne(gomock.Eq(ctx), gomock.Eq(1)).Return(&ent.User{
+		tests := []struct {
+			name string
+			mock func() *mock_service.MockUserService
+			params
+			want want
+		}{
+			{
+				name: "Normal time, can get user",
+				mock: func() *mock_service.MockUserService {
+					m.EXPECT().FindOne(gomock.Eq(ctx), gomock.Eq(1)).Return(&ent.User{
+						ID:    1,
+						Name:  "test user",
+						Email: "test@example.com",
+					}, nil)
+					return m
+				},
+				params: params{id: 1},
+				want: want{user: &ent.User{
 					ID:    1,
 					Name:  "test user",
 					Email: "test@example.com",
-				}, nil)
-				return m
+				}, err: nil},
 			},
-			params: params{id: 1},
-			want: want{user: &ent.User{
-				ID:    1,
-				Name:  "test user",
-				Email: "test@example.com",
-			}, err: nil},
-		},
-		{
-			name: "Error time, can't get user",
-			mock: func() *mock_service.MockUserService {
-				m.EXPECT().FindOne(gomock.Eq(ctx), gomock.Eq(1)).Return(nil, errors.New("some error"))
-				return m
+			{
+				name: "Error time, can't get user",
+				mock: func() *mock_service.MockUserService {
+					m.EXPECT().FindOne(gomock.Eq(ctx), gomock.Eq(1)).Return(nil, errors.New("some error"))
+					return m
+				},
+				params: params{id: 1},
+				want:   want{user: nil, err: errors.New("some error")},
 			},
-			params: params{id: 1},
-			want:   want{user: nil, err: errors.New("some error")},
-		},
-	}
+		}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			qr := &queryResolver{Resolver: &Resolver{userService: tt.mock()}}
-			got, err := qr.User(ctx, tt.params.id)
-			assert.Equal(t, tt.want.user, got)
-			assert.Equal(t, tt.want.err, err)
-		})
-	}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				qr := &queryResolver{Resolver: &Resolver{userService: tt.mock()}}
+				got, err := qr.User(ctx, tt.params.id)
+				assert.Equal(t, tt.want.user, got)
+				assert.Equal(t, tt.want.err, err)
+			})
+		}
+	})
 }
