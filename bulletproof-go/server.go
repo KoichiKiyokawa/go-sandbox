1160c679b8501a420fbea3bafa796ef6111d6c6c
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-05 00:03:32 +0900

    feat: introduce echo

--- bulletproof-go/server.go
+++ bulletproof-go/server.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bulletproof-go/graph"
 	"bulletproof-go/resolver"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
 )
 
 const defaultPort = "8080"
@@ -19,11 +23,36 @@ func main() {
 		port = defaultPort
 	}
 
+	e := echo.New()
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolver.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	e.GET("/graphql", func(c echo.Context) error {
+		playground.Handler("GraphQL playground", "/graphql").ServeHTTP(c.Response().Writer, c.Request())
+		return nil
+	})
+	e.POST("/graphql", func(c echo.Context) error {
+		srv.ServeHTTP(c.Response().Writer, c.Request())
+		return nil
+	})
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// https://echo.labstack.com/cookbook/graceful-shutdown/
+	go func() {
+		if err := e.Start("127.0.0.1:" + port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	log.Printf("connect to http://localhost:%s/grahpql for GraphQL playground", port)
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
